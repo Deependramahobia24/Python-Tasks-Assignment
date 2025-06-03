4348d00e0a28c314365a2772dc71e22f0989da1e
Deependramahobia24 <deependramahobia2004@gmail.com>
2025-06-03 13:40:48 +0530

    Add files via upload

---
+++ assignment_repo.py
@@ -0,0 +1,24 @@
+a = int(input("Enter the first number :"))
+b = int(input("Enter the second number :"))
+
+print("Addition", (a + b))
+print("Subtraction", (a - b))
+print("Multiplication", (a * b))
+print("Division", (a / b))
+
+# output
+# Enter the first number : 5
+# Enter the second number : 10
+# Addition 15
+# Substraction -5
+# multiplication 50
+# Division 0.5
+
+firstname = input("Enter your first name : ")
+lastname = input("Enter your last name : ")
+print("Hello", firstname + " " + lastname, "!", "welcome to python program")
+
+# output
+# Enter your first name : deependra
+# Enter your last name : mahobia
+# Hello deependra mahobia ! welcome to python program
